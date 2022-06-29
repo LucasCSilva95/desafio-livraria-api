ca9b941dd19a3b7360b93d8fff7c0c0605dd8403
Lucas de C. Silva <lucascsilva.dev@gmail.com>
2022-06-28 21:07:44 -0300

    Removidos os arquivos iniciais de exemplo.

--- routes/example.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import ExampleController from "../controllers/example.controller.js";
-
-const router = express.Router();
-
-router.post("/", ExampleController.createExample);
-
-export default router;
--- services/example.service.js
+++
@@ -1,9 +0,0 @@
-import ExampleRepository from "../repositories/example.repository.js";
-
-async function createExample(example) {
-  return await ExampleRepository.insertExample(example);
-}
-
-export default {
-  createExample,
-};
